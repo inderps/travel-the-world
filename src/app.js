c7ff969128bd6e5b2b508902b69b0a074a951ca2
Inderpal Singh <inderps@thoughtworks.com>
2014-06-23 03:43:53 +0530

    added cleartrip api

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   Map.init();
 
   for (var index = 0; index < DESTINATIONS.length; index++) {
-    FareApi.fetch('DEL', DESTINATIONS[index].code, '20140920', index, function(result, i){
-      if(result){
-        Map.addLocation(DESTINATIONS[i].place, "Rs: " + result.fare.totalfare);
+    ClearTripApi.fetch('DEL', DESTINATIONS[index].code, '20140920', index, function(fare, i){
+      if(fare){
+        Map.addLocation(DESTINATIONS[i].place, "Rs: " + fare);
       }
     });
   }
---
+++ src/cleartrip-api.js
@@ -0,0 +1,20 @@
+var ClearTripApi = {
+  fetch: function(src, destination, date, i, callback){
+    $.ajax({
+      url: "http://www.cleartrip.com/flights/calendar/calendarstub.json",
+      type:"get",
+      data:{from: src, to: destination, adults: 1, start_date: date, end_date: date},
+      success: function(response) {
+        var jsonStart = response.responseText.indexOf("<p>") + 3;
+        var jsonEnd = response.responseText.indexOf("</p>");
+        var responseObj = eval("("+response.responseText. substring(jsonStart, jsonEnd)+")");
+        if(responseObj['calendar_json'][date].length>0){
+          callback(_.min(responseObj['calendar_json'][date], function(flight){ return parseFloat(flight.pr); }).pr, i);
+        }
+      },
+      error: function(xhr) {
+        console.log(xhr);
+      }
+    });
+  }
+}
