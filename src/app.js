94ce095a16ed6236124ad516d8f1bb2efce2829e
Inderpal Singh <inderps@thoughtworks.com>
2014-06-23 04:11:18 +0530

    added form to take date input

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
   Map.init();
+  $('#inputDate').datepicker({autoclose: true});
+  $('#choiceModal').modal('show');
+  $('#submitForm').click(function(ev){
+    ev.preventDefault();
+    var date = new Date($("#inputDate").val());
+    var formattedDate = date.getFullYear() + ('0' + (date.getMonth()+1)).slice(-2) + ('0' + date.getDate()).slice(-2);
+    for (var index = 0; index < DESTINATIONS.length; index++) {
+      ClearTripApi.fetch('DEL', DESTINATIONS[index].code, formattedDate, index, function(fare, i){
+        if(fare){
+          Map.addLocation(DESTINATIONS[i].place, "Rs: " + fare);
+        }
+      });
+    }
+    $('#choiceModal').modal('hide');
+  });
 
-  for (var index = 0; index < DESTINATIONS.length; index++) {
-    ClearTripApi.fetch('DEL', DESTINATIONS[index].code, '20140920', index, function(fare, i){
-      if(fare){
-        Map.addLocation(DESTINATIONS[i].place, "Rs: " + fare);
-      }
-    });
-  }
 });
